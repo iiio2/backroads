e3e17704ddc1d7768162bb0c7823490c228bf1fa
Mr. Robot <gomexxxx71@gmail.com>
2023-08-30 19:08:22 +0600

    chore: get year dynamically

--- js/app.js
+++ js/app.js
@@ -4,3 +4,7 @@ const links = document.getElementById("nav-links");
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
+
+// ********** set date ************
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());

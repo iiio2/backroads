e45086100d2f20ebb3ef2033fbc7f0d0bc829b7d
Mr. Robot <gomexxxx71@gmail.com>
2023-08-30 19:05:34 +0600

    chore: navbar toggle

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// ********** navbar toggle ************
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
